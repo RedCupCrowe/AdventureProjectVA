6bc48731c97ba5b9f533c3ead0db3bdcc11ac0c1
RedCupCrowe <valdemarkjohansen@gmail.com>
2021-10-04 18:07:50 +0200

    1.0

--- src/Adventure.java
+++ src/Adventure.java
@@ -1,2 +1,94 @@
+import java.util.Scanner;
+
 public class Adventure {
+    private static Room currentRoom;
+    private static Room room1, room2, room3,room4, room5, room6,room7, room8, room9;
+
+    public static void main(String[] args) {
+        // build all rooms
+        room1 = new Room("1", "INSERT ROOM DESCRIPTION HERE");
+        room2 = new Room("2", "INSERT ROOM DESCRIPTION HERE");
+        room3 = new Room("3", "INSERT ROOM DESCRIPTION HERE");
+        room4 = new Room("4", "INSERT ROOM DESCRIPTION HERE");
+        room5 = new Room("5", "INSERT ROOM DESCRIPTION HERE");
+        room6 = new Room("6", "INSERT ROOM DESCRIPTION HERE");
+        room7 = new Room("7", "INSERT ROOM DESCRIPTION HERE");
+        room8 = new Room("8", "INSERT ROOM DESCRIPTION HERE");
+        room9 = new Room("9", "INSERT ROOM DESCRIPTION HERE");
+        //make all connections
+        room2.setConnections(null, room3, null, room1);
+        room4.setConnections(room1, null, room7, null);
+        room6.setConnections(room3,null, room9, room5);
+        room8.setConnections(null, room9, null, room7);
+       /* room1.setConnections(null, room2, room4, null);
+        room2.setConnections(null, room3, null, room1);
+        room3.setConnections(null, null, room6, room2);
+        room4.setConnections(room1, null, room7, null);
+        room5.setConnections(null, room6, null, null);
+        room6.setConnections(room3, null, room9, room5);
+        room7.setConnections(room4, room8, null, null);
+        room8.setConnections(null, room9, null, room7);
+        room9.setConnections(room6, null, null, room8);*/
+        //set player starting position
+        currentRoom = room1;
+        boolean running = true;
+        Scanner in = new Scanner(System.in);
+        while(running){
+        String input = in.nextLine();
+        switch(input){
+            case "go north":
+            case "n":
+            case "go n":
+                if(currentRoom.getNorth() == null){
+                    System.out.println("you slam your head into the wall");
+                }else{
+                System.out.println("going north");
+                currentRoom = currentRoom.getNorth();}
+                break;
+            case "go south":
+            case "s":
+            case "go s":
+                if(currentRoom.getSouth() == null){
+                    System.out.println("you slam your head into the wall");
+                }else{
+                    System.out.println("going south");
+                    currentRoom = currentRoom.getSouth();}
+                break;
+            case "go east":
+            case "e":
+            case "go e":
+                if(currentRoom.getEast() == null){
+                    System.out.println("you slam your head into the wall");
+                }else{
+                    System.out.println("going east");
+                    currentRoom = currentRoom.getEast();}
+                break;
+            case "go west":
+            case "w":
+            case "go w":
+                if(currentRoom.getWest() == null){
+                    System.out.println("you slam your head into the wall");
+                }else{
+                    System.out.println("going west");
+                    currentRoom = currentRoom.getWest();}
+                break;
+
+            case "look":
+            case "look around":
+            case "peer":
+                System.out.println(currentRoom);
+                break;
+            case "help":
+            case "hlep":
+                System.out.println("INSERT HELP AND TIPS HERE, NUMBNUTS.");
+                break;
+            case "exit":
+                running = false;
+            default:
+                System.out.println("Invalid command");
+                break;
+
+        }
+    }
+}
 }
--- src/Room.java
+++ src/Room.java
@@ -1,2 +1,69 @@
 public class Room {
+    private String name;
+    private Room north;
+    private Room south;
+    private Room east;
+    private Room west;
+    private String roomDescription;
+
+
+    public Room(String name, String roomDescription){
+        this.name = name;
+        this.roomDescription = roomDescription;
+    }
+
+    public void setConnections(Room north, Room east, Room south , Room west){
+        if(north != null)  {setNorth(north);}
+        if(east != null)   {setEast(east);}
+        if(south != null)  {setSouth(south);}
+        if(west != null)   {setWest(west);}
+        /*this.north = north;
+        this.south = south;
+        this.east = east;
+        this.west = west;*/
+
+    }
+
+    public Room getNorth() {
+        return north;
+    }
+
+    public Room getEast() {
+        return east;
+    }
+
+    public Room getSouth() {
+        return south;
+    }
+
+    public Room getWest() {
+        return west;
+    }
+
+    public String toString(){
+
+        return this.name + ": " + this.roomDescription;
+
+    }
+    public void setSouth(Room south){
+        this.south = south;
+        if(south.getNorth() != this){
+        south.setNorth(this);}
+    }
+    public void setNorth(Room north) {
+        this.north = north;
+        if(north.getSouth() != this){
+        north.setSouth(this);}
+    }
+    public void setEast(Room east){
+        this.east = east;
+        if(east.getWest() != this){
+        east.setWest(this);}
+    }
+
+    public void setWest(Room west) {
+        this.west = west;
+        if(west.getEast() != this){
+        west.setEast(this);}
+    }
 }
